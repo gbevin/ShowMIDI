62458ec7ba5dc89747a1bf43058c42062abb1b4c
Geert Bevin <gbevin@uwyn.com>
2023-11-09 09:15:53 -0500

    Tweaks to macOS build script

--- Scripts/build-macos.sh
+++ Scripts/build-macos.sh
@@ -8,27 +8,30 @@ fi
 export RELEASE_VERSION="$1"
 export PATH_TO_JUCE=/Users/gbevin/JUCE
 
-rm -rfv Builds/MacOSX/build/
+BUILD_DIR=$PWD/Builds/MacOSX/build
+rm -rfv $BUILD_DIR
 
 # build release artifacts
 xcodebuild -project ./Builds/MacOSX/showmidi.xcodeproj -config Release SYMROOT=build -scheme "ShowMIDI - All"
 
 cmake -BBuilds/MacOSX/build/clap -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"
-cmake --build Builds/MacOSX/build/clap --config Release
+cmake --build $BUILD_DIR/clap --config Release
 
 # update packager file with version number and generate it in the build
 # directory, updating relevant paths
 sed "s/REPLACE_VERSION/$RELEASE_VERSION/g" Installers/ShowMIDI.pkgproj | \
-    sed "s/Builds\/MacOSX/..\/..\/Builds\/MacOSX/g" > Builds/MacOSX/build/ShowMIDI_versioned.pkgproj
+    sed "s/Builds\/MacOSX/..\/..\/Builds\/MacOSX/g" > $BUILD_DIR/ShowMIDI_versioned.pkgproj
 # copy other files that are needed by the installer to the new location
-cp Installers/uninstall.sh Builds/MacOSX/build/
-cp Installers/gpl-3.0.txt Builds/MacOSX/build/
+cp Installers/uninstall.sh $BUILD_DIR
+cp Installers/gpl-3.0.txt $BUILD_DIR
 
 # build the installer package in the build directory
-/usr/local/bin/packagesbuild --project Builds/MacOSX/build/ShowMIDI_versioned.pkgproj -v
-mv Builds/MacOSX/build/ShowMIDI.pkg Builds/MacOSX/build/ShowMIDI-${RELEASE_VERSION}.pkg
+/usr/local/bin/packagesbuild --project $BUILD_DIR/ShowMIDI_versioned.pkgproj -v
+mv $BUILD_DIR/ShowMIDI.pkg $BUILD_DIR/ShowMIDI-${RELEASE_VERSION}.pkg
 
-ARCHIVE_FILE="Builds/MacOSX/build/ShowMIDI-macOS-${RELEASE_VERSION}.zip"
-zip -r  "${ARCHIVE_FILE}" "Builds/MacOSX/build/ShowMIDI-${RELEASE_VERSION}.pkg"
+ARCHIVE_FILE="$BUILD_DIR/ShowMIDI-macOS-${RELEASE_VERSION}.zip"
+pushd "$BUILD_DIR"
+zip -r  "${ARCHIVE_FILE}" "ShowMIDI-${RELEASE_VERSION}.pkg"
+popd
 
 echo "Finished building `realpath $ARCHIVE_FILE`"
