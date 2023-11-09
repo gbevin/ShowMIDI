2f55e1206c86d77814e962ecbe96ec37c031d549
Geert Bevin <gbevin@uwyn.com>
2023-11-08 21:27:39 -0500

    Added support for clap plugin format on Linux

--- Scripts/build-linux.sh
+++ Scripts/build-linux.sh
@@ -5,7 +5,7 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-RELEASE_VERSION="$1"
+export RELEASE_VERSION="$1"
 
 rm -rfv Builds/LinuxMakefile/build/
 
@@ -13,12 +13,18 @@ PROJECT_DIR=$PWD
 
 pushd Builds/LinuxMakefile
 make Config=Release
+popd
+
+cmake -BBuilds/LinuxMakefile/build/clap -DCMAKE_BUILD_TYPE=Release
+pushd Builds/LinuxMakefile
+cmake --build build/clap --config Release
 
 STAGE_DIR="build/ShowMIDI-$RELEASE_VERSION"
-mkdir -p $STAGE_DIR/vst $STAGE_DIR/vst3
-mv build/showmidi $STAGE_DIR
-mv build/showmidi.so $STAGE_DIR/vst
-mv build/showmidi.vst3 $STAGE_DIR/vst3
+mkdir -p $STAGE_DIR/vst $STAGE_DIR/vst3 $STAGE_DIR/clap
+mv build/ShowMIDI $STAGE_DIR
+mv build/ShowMIDI.so $STAGE_DIR/vst
+mv build/ShowMIDI.vst3 $STAGE_DIR/vst3
+mv build/clap/ShowMIDI_artefacts/Release/ShowMIDI.clap $STAGE_DIR/clap
 cp $PROJECT_DIR/*.md $STAGE_DIR
 
 ARCHIVE_FILE="build/showmidi-linux-ubuntu-x64-$RELEASE_VERSION.tar.bz2"
