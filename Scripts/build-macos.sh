00bfe35a92d480d961e40e52f6fff0076f1eb450
Geert Bevin <gbevin@uwyn.com>
2023-11-08 21:36:51 -0500

    Simplified macos build script

--- Scripts/build-macos.sh
+++ Scripts/build-macos.sh
@@ -15,9 +15,10 @@ xcodebuild -project ./Builds/MacOSX/showmidi.xcodeproj -config Release SYMROOT=b
 # update packager file with version number and generate it in the build
 # directory, updating relevant paths
 sed "s/REPLACE_VERSION/$RELEASE_VERSION/g" Installers/ShowMIDI.pkgproj | \
-    sed "s/Builds\/MacOSX/..\/..\/Builds\/MacOSX/g" | \
-    sed "s/uninstall.sh/..\/..\/..\/Installers\/uninstall.sh/g" | \
-    sed "s/gpl-3.0.txt/..\/..\/..\/Installers\/gpl-3.0.txt/g" > Builds/MacOSX/build/ShowMIDI_versioned.pkgproj
+    sed "s/Builds\/MacOSX/..\/..\/Builds\/MacOSX/g" > Builds/MacOSX/build/ShowMIDI_versioned.pkgproj
+# copy other files that are needed by the installer to the new location
+cp Installers/uninstall.sh Builds/MacOSX/build/
+cp Installers/gpl-3.0.txt Builds/MacOSX/build/
 
 # build the installer package in the build directory
 /usr/local/bin/packagesbuild --project Builds/MacOSX/build/ShowMIDI_versioned.pkgproj -v
