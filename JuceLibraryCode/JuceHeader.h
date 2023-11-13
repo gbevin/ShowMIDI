16783f0b964440ace7752da4e62672fdce69715f
Geert Bevin <gbevin@uwyn.com>
2023-11-12 20:51:33 -0500

    Updated version to 0.5.1 build 6

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -49,7 +49,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "ShowMIDI";
     const char* const  companyName    = "Uwyn";
-    const char* const  versionString  = "0.5.0";
-    const int          versionNumber  = 0x500;
+    const char* const  versionString  = "0.5.1";
+    const int          versionNumber  = 0x501;
 }
 #endif
