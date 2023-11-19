5f9987fb046b122609fbb99d2921aeabd977a647
Geert Bevin <gbevin@uwyn.com>
2023-11-19 15:37:15 -0500

    Updated version to 0.8.0 build 11. Added copyright statement.

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -49,7 +49,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "ShowMIDI";
     const char* const  companyName    = "Uwyn";
-    const char* const  versionString  = "0.7.0";
-    const int          versionNumber  = 0x700;
+    const char* const  versionString  = "0.8.0";
+    const int          versionNumber  = 0x800;
 }
 #endif
